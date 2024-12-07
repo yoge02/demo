a4ec71b545d53ea6e9f76b756cb625cae5cb8abe
yoge02 <yoge60@gmail.com>
2024-12-07 20:13:39 +0000

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,14 @@
+package practise;
+
+public class condition {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num<9.6){
+            System.out.println("greater");
+        } else if (num>9) {
+            System.out.println("smaller");
+        }else {
+            System.out.println("unknown");
+        }
+    }
+}
